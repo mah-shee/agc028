e03b7c17069a892f08313ca85f7794f1480092da
Masa <masa@masa.local>
2020-08-20 20:33:53 +0900

    Initial commit

--- src/bin/a.rs
+++ src/bin/a.rs
@@ -1,8 +1,18 @@
+use num_integer::{gcd, lcm};
 #[allow(unused_imports)]
 use proconio::marker::{Bytes, Chars, Usize1};
 use proconio::{fastout, input};
 #[fastout]
 fn main() {
     input! {
+        n: usize,
+        m: usize,
+        s: Chars,
+        t: Chars,
     }
+    let lcm = lcm(n, m);
+    let gcd = gcd(n, m);
+    let mut ans: String = String::new();
+
+    println!("{}", ans.len());
 }
